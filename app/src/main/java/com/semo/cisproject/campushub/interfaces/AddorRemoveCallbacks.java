94ad460a6a7abee5e5bf52dad40bf495d324a499
Trent Moore <tmmoore5s@semo.edu>
2026-04-05 19:35:47 -0500

    Refactor: Cleaned up template, renamed package, and applied CampusHub branding

---
+++ app/src/main/java/com/semo/cisproject/campushub/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,13 @@
+package com.semo.cisproject.campushub.interfaces;
+
+
+
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
